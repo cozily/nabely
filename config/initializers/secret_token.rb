124b8c4e1f6fb8cffd3411dcd1290e846da1ea77
Todd Persen <todd.persen@gmail.com>
2012-03-07 00:35:34 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nabely::Application.config.secret_token = '2e97b536ce8dd6aac81bb632bc1f623dfc62448e17b3ad94e2baa57fc39056e76bbf95f6966652a1e763f4d9aef9c2a86732d13d0faf97b3f94bb9349c217fe6'
